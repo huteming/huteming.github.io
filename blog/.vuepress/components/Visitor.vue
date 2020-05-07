74dc069a3a944d62a470761ecd8603c127b2db09
huteming <549270031@qq.com>
2020-05-07 21:56:04 +0800

    添加valine

---
+++ blog/.vuepress/components/Visitor.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="valine-vuepress">
+    <div class="leancloud_visitors">
+      <!-- <em class="post-meta-item-text">阅读量： </em> -->
+      <!-- <i class="leancloud-visitors-count">100</i> -->
+    </div>
+  </div>
+</template>
--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -32,10 +32,33 @@ module.exports = {
         },
       },
     ],
+    // 谷歌分析
     [
       '@vuepress/google-analytics',
       {
         'ga': 'UA-139065266-1' // UA-00000000-0
+      }
+    ],
+    // 评论系统 valine
+    // https://valine.js.org/vuepress.html
+    [
+      'vuepress-plugin-comment',
+      {
+        choosen: 'valine', 
+        // options选项中的所有参数，会传给Valine的配置
+        options: {
+          el: '#valine-vuepress',
+          appId: 'oPQ3dJVj0Ltg62BaVlONulv3-gzGzoHsz',
+          appKey: 'Fo3CGQAkNSqxmtx8WsHuozdX',
+          visitor: true, // 阅读量统计
+        }
+      }
+    ],
+    // 自定义组件
+    [
+      '@vuepress/register-components',
+      {
+        componentsDir: './components',
       }
     ],
   ],
@@ -47,13 +70,7 @@ module.exports = {
       { text: '编程', link: '/' },
       { text: '原理解析', link: '/theories/' },
       { text: '工具指南', link: '/tools/' },
-      { text: '算法', link: '/algorithm/' },
-      // {
-      //   text: '手写实现',
-      //   items: [
-      //     { text: 'Chinese', link: '/language/chinese' },
-      //   ]
-      // }
+      { text: '算法', link: '/algorithm/' }
     ],
     directories: [
       {
@@ -89,29 +106,7 @@ module.exports = {
       lengthPerPage: 5,
     },
     sitemap: {
-      hostname: 'https://huteming.site',
+      hostname: 'https://blog.huteming.site',
     },
-
-    // 页面底部显示最后更新时间
-    lastUpdated: 'Last Updated', // string | boolean
-
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'huteming.github.io',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    // repoLabel: '查看源码',
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: 'vuejs/vuepress',
-    // 假如文档不是放在仓库的根目录下：
-    // docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '去 Github 指出我的错误！',
   },
 }
