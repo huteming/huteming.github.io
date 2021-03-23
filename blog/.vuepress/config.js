3feebf11574ae7a1ed873d0ba85f599bdd35a6ab
huteming <549270031@qq.com>
2021-03-23 18:11:27 +0800

    chores: 调整目录

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,50 +1,48 @@
 module.exports = {
-  title: '特明的日志',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ],
+  title: "特明的日志",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
 
   plugins: [
     // 自定义容器: https://v1.vuepress.vuejs.org/zh/guide/markdown.html#%E8%87%AA%E5%AE%9A%E4%B9%89%E5%AE%B9%E5%99%A8
     // 自定义容器: 引用
     [
-      'vuepress-plugin-container',
+      "vuepress-plugin-container",
       {
-        type: 'right',
-        defaultTitle: ' ',
+        type: "right",
+        defaultTitle: " ",
       },
     ],
     [
-      'vuepress-plugin-container',
+      "vuepress-plugin-container",
       {
-        type: 'theorem',
-        before: info => `<div class="theorem"><p class="title">${info}</p>`,
-        after: '</div>',
+        type: "theorem",
+        before: (info) => `<div class="theorem"><p class="title">${info}</p>`,
+        after: "</div>",
       },
     ],
     // 自定义容器: 状态提示
     [
-      'vuepress-plugin-container',
+      "vuepress-plugin-container",
       {
-        type: 'tip',
+        type: "tip",
         defaultTitle: {
-          '/': '提示',
+          "/": "提示",
         },
       },
     ],
     // 谷歌分析
     [
-      '@vuepress/google-analytics',
+      "@vuepress/google-analytics",
       {
-        'ga': 'UA-139065266-1' // UA-00000000-0
-      }
+        ga: "UA-139065266-1", // UA-00000000-0
+      },
     ],
     // 评论系统 valine
     // https://valine.js.org/vuepress.html
     // [
     //   'vuepress-plugin-comment',
     //   {
-    //     choosen: 'valine', 
+    //     choosen: 'valine',
     //     // options选项中的所有参数，会传给Valine的配置
     //     options: {
     //       el: '#valine-vuepress',
@@ -56,65 +54,65 @@ module.exports = {
     // ],
     // 自定义组件
     [
-      '@vuepress/register-components',
+      "@vuepress/register-components",
       {
-        componentsDir: './components',
-      }
+        componentsDir: "./components",
+      },
     ],
   ],
 
-  theme: '@vuepress/theme-blog',
+  theme: "@vuepress/theme-blog",
   themeConfig: {
-    dateFormat: 'YYYY-MM-DD',
+    dateFormat: "YYYY-MM-DD",
     nav: [
-      { text: '编程', link: '/' },
-      { text: '算法', link: '/algorithm/' },
-      { text: '知识点', link: '/knowledge/' },
-      { text: '原理解析', link: '/theories/' },
-      { text: '工具指南', link: '/tools/' },
+      { text: "编程", link: "/" },
+      // { text: '算法', link: '/algorithm/' },
+      { text: "知识点", link: "/knowledge/" },
+      // { text: '原理解析', link: '/theories/' },
+      { text: "工具指南", link: "/tools/" },
     ],
     directories: [
       {
-        id: 'algorithm',
-        dirname: '_algorithm',
-        path: '/algorithm/',
-        title: '算法',
-        itemPermalink: '/algorithm/:slug',
+        id: "algorithm",
+        dirname: "_algorithm",
+        path: "/algorithm/",
+        title: "算法",
+        itemPermalink: "/algorithm/:slug",
       },
       {
-        id: 'knowledge',
-        dirname: '_knowledge',
-        path: '/knowledge/',
-        title: '知识点',
-        itemPermalink: '/knowledge/:slug',
+        id: "knowledge",
+        dirname: "_knowledge",
+        path: "/knowledge/",
+        title: "知识点",
+        itemPermalink: "/knowledge/:slug",
       },
       {
-        id: 'coding',
-        dirname: '_coding',
-        path: '/',
-        title: '编程',
-        itemPermalink: '/coding/:slug',
+        id: "coding",
+        dirname: "_coding",
+        path: "/",
+        title: "编程",
+        itemPermalink: "/coding/:slug",
       },
       {
-        id: 'theories',
-        dirname: '_theories',
-        path: '/theories/',
-        title: '原理解析',
-        itemPermalink: '/theories/:slug',
+        id: "theories",
+        dirname: "_theories",
+        path: "/theories/",
+        title: "原理解析",
+        itemPermalink: "/theories/:slug",
       },
       {
-        id: 'tools',
-        dirname: '_tools',
-        path: '/tools/',
-        title: '工具指南',
-        itemPermalink: '/tools/:slug',
+        id: "tools",
+        dirname: "_tools",
+        path: "/tools/",
+        title: "工具指南",
+        itemPermalink: "/tools/:slug",
       },
     ],
     globalPagination: {
       lengthPerPage: 5,
     },
     sitemap: {
-      hostname: 'https://huteming.github.io',
+      hostname: "https://huteming.github.io",
     },
   },
-}
+};
