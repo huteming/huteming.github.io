1cd9c2fd9b8578a2cd67be9bece2f49ee86a2a91
huteming <549270031@qq.com>
2021-07-09 18:44:58 +0800

    feat: 添加 travis-ci 部署环境

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 module.exports = {
   title: '特明的日志',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://huteming.github.io', // 您的网站网址
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'huteming', // Usually your GitHub org/user name.
+  projectName: 'huteming.github.io', // Usually your repo name.
+  trailingSlash: false,
   themeConfig: {
     navbar: {
       title: '特明的日志',
