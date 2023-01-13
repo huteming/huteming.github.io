4335f0ce31b03551cbd3aaa897c7b3e5ef83da8c
huteming <teming.hu@qq.com>
2023-01-13 16:12:09 +0800

    feat: 布局、主题等配置修改为v2.2.0默认配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,22 +12,16 @@ module.exports = {
   onBrokenLinks: 'throw', // 检测到无效链接时的行为
   onBrokenMarkdownLinks: 'warn', // 在检测到无效 Markdown 链接时的行为
   favicon: 'img/favicon.ico',
+  trailingSlash: false, // 允许您自定义 URL/链接后是否添加结尾斜杠。
+
   organizationName: 'huteming', // 拥有此源的 GitHub 用户或组织。 用于部署命令。
   projectName: 'huteming.github.io', // GitHub 源的名称。 用于部署命令。
-  trailingSlash: false, // 允许您自定义 URL/链接后是否添加结尾斜杠。
+
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
   },
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
+
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -45,22 +39,19 @@ module.exports = {
           showReadingTime: true,
         },
         theme: {
-          customCss: [
-            require.resolve('./src/css/customTheme.scss'),
-            require.resolve('./src/css/index.scss'),
-            require.resolve('./src/css/showcase.scss'),
-            require.resolve('./src/css/versions.scss'),
-            require.resolve('./src/css/atom.scss'),
-            // require.resolve('./src/css/custom.css'),
-          ],
+          customCss: [require.resolve('./src/css/custom-v2.2.0.css')],
         },
       },
     ],
   ],
+
   themeConfig: {
     navbar: {
       title: '笔记',
-      style: 'dark', // dark | primary
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.png',
+      },
       items: directories.map(({ label, docId }) => ({
         type: 'doc',
         position: 'right',
@@ -68,11 +59,12 @@ module.exports = {
         docId,
       })),
     },
+
     prism: {
-      theme: darkCodeTheme,
+      theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
   },
+
   themes: ['@docusaurus/theme-live-codeblock'],
-  plugins: ['docusaurus-plugin-sass'],
 }
