2991cbc8fcf21235396b065ef44540d4f565b364
Ando.Hu <ando.hu@zoom.us>
2024-01-26 13:42:49 +0800

    feat: 列表支持筛选

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,12 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import useGlobalData from '@docusaurus/useGlobalData'
 import type { BlogPost } from '@docusaurus/plugin-content-blog'
-import { log } from '../utils'
+import { fuzzyMatch, log } from '../utils'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import TextField from '@mui/material/TextField'
 
 import styles from './index.module.scss'
 
@@ -71,18 +76,66 @@ export default function Home(): JSX.Element {
 
   const globalData = useGlobalData()
   const blogPluginData: any = globalData?.['docusaurus-plugin-content-blog']?.['default']
-  const { blogs } = blogPluginData
+  const { blogs, tags } = blogPluginData
   log('全局博客的内容：', blogPluginData)
+
+  const [searchValue, setSearchValue] = React.useState('')
+  const [selectedTag, setSelectedTag] = React.useState('')
+
+  const tagOptions = React.useMemo(() => {
+    return Object.values(tags).map((tag: any) => tag.label)
+  }, [tags])
+
+  const renderBlogs = React.useMemo(() => {
+    return blogs.filter((blog: any) => {
+      const isSearch = fuzzyMatch(blog?.metadata?.title, searchValue)
+      const isTag = !selectedTag || (selectedTag && blog?.metadata?.frontMatter?.tags?.includes(selectedTag))
+      return isSearch && isTag
+    })
+  }, [blogs, searchValue, selectedTag])
 
   return (
     <Layout description={siteConfig.tagline}>
       <main className={styles.wrapper}>
-        {/* <header className={styles.header}>
-          <div className={styles.headerFilter}>Feed settings</div>
-        </header> */}
+        <header className={styles.header}>
+          <div className={styles.headerFilter}>
+            <TextField
+              label='标题'
+              id='title-input'
+              size='small'
+              value={searchValue}
+              onChange={(event) => {
+                setSearchValue(event.target.value)
+              }}
+            />
+
+            <FormControl sx={{ minWidth: 120 }} size='small'>
+              <InputLabel id='select-tag-label'>标签</InputLabel>
+              <Select
+                labelId='select-tag-label'
+                label='Tag'
+                id='select-tag'
+                value={selectedTag}
+                onChange={(event) => {
+                  setSelectedTag(event.target.value)
+                }}
+              >
+                <MenuItem value=''>
+                  <em>None</em>
+                </MenuItem>
+
+                {tagOptions.map((tagStr) => (
+                  <MenuItem key={tagStr} value={tagStr}>
+                    {tagStr}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </header>
 
         <div className={styles.content}>
-          {blogs.map((blog) => (
+          {renderBlogs.map((blog) => (
             <Card blog={blog} key={blog.id} />
           ))}
         </div>
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,22 @@
+import { Nullable } from './types'
+
+export function fuzzyMatch(word: Nullable<string>, searchValue: Nullable<string>): boolean {
+  word = word?.trim()
+  searchValue = searchValue?.trim()
+
+  if (!searchValue) {
+    return true
+  }
+  if (!word) {
+    return false
+  }
+
+  word = word.toLowerCase()
+  searchValue = searchValue.toLowerCase()
+
+  return word.includes(searchValue)
+}
+
 export function isDevelopment(): boolean {
   return process.env.NODE_ENV === 'development'
 }
