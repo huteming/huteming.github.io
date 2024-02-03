b2f2bd3312387cad67157dc327b2188ed7f1e23c
hutm <teming.hu@qq.com>
2024-02-03 18:43:22 +0800

    chore: 拆分 card 到组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,16 @@
 import React from 'react'
-import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import useGlobalData from '@docusaurus/useGlobalData'
-import type { BlogPost } from '@docusaurus/plugin-content-blog'
 import { fuzzyMatch, log } from '../utils'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Select from '@mui/material/Select'
 import TextField from '@mui/material/TextField'
+import IndexCard from '@site/src/components/IndexCard'
 
 import styles from './index.module.scss'
-
-interface ICardProps {
-  blog: BlogPost
-}
-
-function Card(props: ICardProps) {
-  const { blog } = props
-  const { metadata } = blog
-  const { permalink, title, readingTime, formattedDate, frontMatter } = metadata
-  const { tags = [], image } = frontMatter
-
-  const imgBind = (() => {
-    for (const tag of tags) {
-      const tagStr = typeof tag === 'string' ? tag : tag.label
-
-      try {
-        const icon = require(`@site/static/site/tag-icon/${tagStr}.png`).default
-        return {
-          alt: tagStr,
-          src: icon,
-        }
-      } catch (err) {
-        // console.error(err)
-      }
-    }
-
-    return {
-      alt: 'code',
-      src: require('@site/static/site/tag-icon/code.png').default,
-    }
-  })()
-
-  // https://res.cloudinary.com/daily-now/image/upload/f_auto/v1/placeholders/1
-  // https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/61b95af3fa2a7c781ee52899fcaba008?_a=AQAEufR
-  const cover = image || 'https://res.cloudinary.com/daily-now/image/upload/f_auto/v1/placeholders/1'
-
-  return (
-    <article className={styles.card}>
-      <div className={styles.cardHeader}>
-        <img className={styles.cardIcon} {...imgBind} />
-
-        <div className={styles.cardTitle}>
-          <Link to={permalink}>{title}</Link>
-        </div>
-
-        <div className={styles.cardTip}>
-          {formattedDate} • 阅读需 {Math.ceil(readingTime) || 1} 分钟
-        </div>
-      </div>
-
-      <img className={styles.cardCover} alt='' src={cover} />
-
-      <div className={styles.cardFooter}>
-        <i className='fa-regular fa-share-from-square'></i>
-      </div>
-    </article>
-  )
-}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
@@ -136,7 +77,7 @@ export default function Home(): JSX.Element {
 
         <div className={styles.content}>
           {renderBlogs.map((blog) => (
-            <Card blog={blog} key={blog.id} />
+            <IndexCard blog={blog} key={blog.id} />
           ))}
         </div>
       </main>
