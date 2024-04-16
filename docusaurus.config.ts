0169d803c54d69becbd241611ed90285a0f6f143
Ando.Hu <ando.hu@zoom.us>
2024-04-16 10:57:41 +0800

    feat: 新增顶部菜单 roadmap

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,10 +58,16 @@ const config: Config = {
       },
       items: [
         {
+          type: 'docSidebar', // 把这个项目的类型设置为侧边栏链接
+          position: 'left',
           label: 'Docs',
-          sidebarId: 'docsSidebar',
+          sidebarId: 'docsSidebar', // 可以将一个导航栏项目链接到某个给定侧边栏的第一个文档链接
+        },
+        {
           type: 'docSidebar',
           position: 'left',
+          label: 'Roadmap',
+          sidebarId: 'roadmap',
         },
       ],
     },
