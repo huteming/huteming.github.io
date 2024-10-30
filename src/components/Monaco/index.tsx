dabee34507654c9dddfbee3a8d5387e2ea6b1ecd
Ando.Hu <ando.hu@zoom.us>
2024-10-30 17:02:00 +0800

    fix: 修复 monaco 引起的打包异常

--- src/components/Monaco/index.tsx
+++ src/components/Monaco/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import * as monaco from 'monaco-editor'
+// import * as monaco from 'monaco-editor'
+import Editor from '@monaco-editor/react'
 
 const languageMap = {
   'language-ts': 'typescript',
@@ -12,22 +13,22 @@ interface Props {
 
 const Monaco: React.FC<Props> = (props) => {
   const { code, className } = props
-  const editorRef = React.useRef(null)
+  // const editorRef = React.useRef(null)
 
-  React.useEffect(() => {
-    // doc: https://microsoft.github.io/monaco-editor/docs.html
-    const myEditor = monaco.editor.create(editorRef.current, {
-      value: code,
-      language: languageMap[className] || 'javascript',
-      automaticLayout: true,
-      scrollBeyondLastLine: false,
-      fontSize: 16,
-    })
+  // React.useEffect(() => {
+  //   // doc: https://microsoft.github.io/monaco-editor/docs.html
+  //   const myEditor = monaco.editor.create(editorRef.current, {
+  //     value: code,
+  //     language: languageMap[className] || 'javascript',
+  //     automaticLayout: true,
+  //     scrollBeyondLastLine: false,
+  //   })
+  //   return myEditor.dispose
+  // }, [code])
 
-    return () => myEditor.dispose()
-  }, [code])
+  // return <div ref={editorRef} style={{ height: '300px' }} />
 
-  return <div ref={editorRef} style={{ height: '400px' }} />
+  return <Editor height='300px' value={code} language={languageMap[className] || 'javascript'} />
 }
 
 export default Monaco
